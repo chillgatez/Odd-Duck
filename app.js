8b87889389a11171c31e490314c9f25b5f6ab72c
Kelsee Watts <123975076+chillgatez@users.noreply.github.com>
2023-03-21 22:08:24 -0500

    started lab 11

---
+++ app.js
@@ -0,0 +1,10 @@
+"use strict"
+
+let productContainer = document.querySelector('section');
+let reults = document.querySelector('section + p');
+let firstImage = document.querySelector('section img:first-child');
+let secondImage = document.querySelector('section img:nth-child(2)');
+let thirdImage = document.querySelector('section img:last-child')
+
+
+
